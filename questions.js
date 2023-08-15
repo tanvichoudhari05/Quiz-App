16691e63d50952f629044d2e62a3c9da2ebdb20f
tanvi choudhari <tanvichoudhari6957@gmail.com>
2023-08-15 13:01:40 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,68 @@
+let  questions=[
+    {
+        numb: 1,
+        question:'Who is the father of C language?',
+        answer:'Dennis Ritchie',
+        options:[
+            'Steve Jobs',
+            'James Gosling',
+            'Dennis Ritchie',
+           'Rasmus Lerdorf'
+        ]
+    },
+    {
+        numb: 2,
+        question:' All keywords in C are in ____________',
+        answer:' LowerCase letters',
+        options:[
+            ' LowerCase letters',
+            ' UpperCase letters',
+            ' CamelCase letters',
+            'None of the mentioned'
+        ]
+    },
+    {
+        numb: 3,
+        question:' Which of the following is not a valid C variable name?',
+        answer:'int $main',
+        options:[
+            ' int number',
+            ' floar rate',
+            ' int variable_count',
+           'int $main'
+        ]
+    },
+    {
+        numb: 4,
+        question:'Which is valid C expression?',
+        answer:' int my_num = 100000;',
+        options:[
+            'int my_num = 100,000;',
+            'int my_num = 100000;',
+            'int my num = 1000;',
+           ' int $my_num = 10000;'
+        ]
+    },
+    {
+        numb: 5,
+        question:'Which of the following cannot be a variable name in C?',
+        answer:'volatile',
+        options:[
+            'true',
+            'export',
+            'friend',
+           'volatile'
+        ]
+    },
+    {
+        numb: 6,
+        question:'Which keyword is used to prevent any changes in the variable within a C program?',
+        answer:' const',
+        options:[
+            'const',
+            'immutable',
+            'mutable',
+           'volatile'
+        ]
+    }
+];
